49401c319d490ab90335e35201f427ab6f968012
kosui3625 <154409416+kosui3625@users.noreply.github.com>
2024-01-24 22:28:43 +0900

    aplication_controller　～ バリデーション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :set_prototype, except: [:index, :new]
-  before_action :authenticate_user!, except: [:index, :show]
+  #before_action :set_prototype, except: [:index, :new]
+  #before_action :authenticate_user!, except: [:index, :show]
 
   def index
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,9 @@ class User < ApplicationRecord
   validates :first_name_hiragana, presence: true
   validates :birth, presence: true
 
+  validates :password, presence: true, length: { minimum: 6}, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: "は半角英数字混合で入力してください" 
+  validates :family_name_hiragana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "は全角（カタカナ）で入力してください" }
+  validates :first_name_hiragana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "は全角（カタカナ）で入力してください" }
+  validates :name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龯々ー]+\z/, message: "は全角の漢字・ひらがな・カタカナで入力してください" }
+
 end
