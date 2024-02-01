42f5c375b7f54350dbd2e378aaa1f0a1038481ad
kosui3625 <154409416+kosui3625@users.noreply.github.com>
2024-02-01 14:11:59 +0900

    修正03

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :users, only: [:new, :edit, :update ]
+  resources :users, only: [:new, :edit] #:update ]
   #resources :items, only: [:index, :new, :create, :update]
 
   
