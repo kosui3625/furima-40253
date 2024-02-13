abf522f6855e35792fc1c0df170f676802f14036
kosui3625 <154409416+kosui3625@users.noreply.github.com>
2024-02-13 11:02:58 +0900

    Js計算修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,13 +3,13 @@ const item_price = () => {
   priceInput.addEventListener("input", () => {
     const addTaxDom = document.getElementById("add-tax-price");
     const price = parseInt(priceInput.value);
-    const commission = price * 0.1;
+    const commission = Math.floor(price * 0.1);
     addTaxDom.innerHTML = commission.toFixed(0);
   });
   priceInput.addEventListener("input", () => {
     const addTaxDom = document.getElementById("profit");
     const price = parseInt(priceInput.value);
-    const commission = price * 0.9;
+    const commission = Math.floor(price * 0.9);
     addTaxDom.innerHTML = commission.toFixed(0);
   });
 };
